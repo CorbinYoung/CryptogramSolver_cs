194ae78bffa5bea167d332b71f51e6aff56d13c3
CorbinYoung <corbin.young@eagles.oc.edu>
2018-07-11 15:44:29 -0500

    Forgot to add Runner.cs file last time. It is now being added

---
+++ src/test/com/cryptogram/solver/cs/Runner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CryptoSolver.main.com.cryptogram.solver.cs;
+
+namespace CryptoSolver.test.com.cryptogram.solver.cs {
+    
+    public class Runner {
+        public static void Main(string[] args) {
+            var mySolver = Solver.GetInstance();
+
+            var result = mySolver.Solve("congrats.txt");
+
+            Console.WriteLine("1st encrypted message:");
+            Console.WriteLine(mySolver.GetEncryptedMsg());
+
+            Console.WriteLine("1st decrypted message:");
+            Console.WriteLine(result);
+
+            var result2 = mySolver.Solve("preamble.txt");
+
+            Console.WriteLine("\n2nd encrypted message:");
+            Console.WriteLine(mySolver.GetEncryptedMsg());
+
+            Console.WriteLine("2nd decrypted message:");
+            Console.WriteLine(result2);
+        }
+    }
+}
