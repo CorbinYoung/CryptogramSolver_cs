c63de2a8e9bd1e20ad80a9649cafba6dfd06dfe1
CorbinYoung <corbin.young@eagles.oc.edu>
2018-07-12 09:31:05 -0500

    All of the cs files have been moved to this repository

---
+++ CryptoSolver/main.com.cryptogram.solver/Index.cs
@@ -0,0 +1,32 @@
+﻿namespace CryptoSolver.main.com.cryptogram.solver {
+    
+    /*
+     * This class holds the indexes for a current pair
+     *  one encrypted word = one decrypted word
+     *
+     * @author Corbin Young
+     */
+    internal sealed class Index {
+        private readonly int _wI;     //index for the encrypted word
+        private readonly int _mI;     //index for the matching decrypted word
+        
+        /*
+         * Creates a new {@code Index} with the given indexes for the pair
+         *
+         * @param word index for the encrypted word
+         * @param match index for the decrypted word
+         */
+        public Index(int word, int match) {
+            _wI = word;
+            _mI = match;
+        }
+
+        public int GetWi() {
+            return _wI;
+        }
+
+        public int GetMi() {
+            return _mI;
+        }
+    }
+}
---
+++ CryptoSolver/main.com.cryptogram.solver/IndexStack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoSolver.main.com.cryptogram.solver {
+    
+    internal sealed class IndexStack : Stack<Index> {
+        private static readonly IndexStack Instance = new IndexStack();
+
+        private IndexStack() {
+        }
+
+        public static IndexStack GetInstance() {
+            return Instance;
+        }
+    }
+}
