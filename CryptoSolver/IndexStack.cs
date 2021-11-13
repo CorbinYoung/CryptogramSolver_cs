24e4e0d7c482b4a99ba7c8ab03f0e30235011b72
Corbin Young <cog467@gmail.com>
2021-11-12 23:13:40 -0600

    Created actual solution and project for the repo

---
+++ CryptoSolver/IndexStack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CryptoSolver {
+
+  internal sealed class IndexStack : Stack<Index> {
+    private static readonly IndexStack Instance = new IndexStack();
+
+    private IndexStack() {
+    }
+
+    public static IndexStack GetInstance() {
+      return Instance;
+    }
+  }
+}
---
+++ CryptoSolver/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CryptoSolver {
+
+  public class Program {
+
+    public static void Main(string[] args) {
+      var mySolver = Solver.GetInstance();
+
+      var result = mySolver.Solve($"{Directory.GetCurrentDirectory()}\\..\\..\\..\\congrats.txt");
+
+      Console.WriteLine("1st encrypted message:");
+      Console.WriteLine(mySolver.GetEncryptedMsg());
+
+      Console.WriteLine("1st decrypted message:");
+      Console.WriteLine(result);
+
+      Console.WriteLine("\nKey:");
+      Console.WriteLine(mySolver.DisplayKey());
+
+      var result2 = mySolver.Solve($"{Directory.GetCurrentDirectory()}\\..\\..\\..\\preamble.txt");
+
+      Console.WriteLine("\n2nd encrypted message:");
+      Console.WriteLine(mySolver.GetEncryptedMsg());
+
+      Console.WriteLine("2nd decrypted message:");
+      Console.WriteLine(result2);
+
+      Console.WriteLine("\nKey:");
+      Console.WriteLine(mySolver.DisplayKey());
+    }
+  }
+}
